909ee3e2b89edd367b5f03b1279addac9ad022c7
Alex <alexrkoch@gmail.com>
2024-12-01 15:20:01 -0600

    day 1 part 2, not optimized

--- days/1/day-1.test.ts
+++ days/1/day-1.test.ts
@@ -1,7 +1,11 @@
 import { expect, test } from "bun:test";
 import testInput from "./test-input.txt";
-import day1 from "./day-1";
+import { day1part1, day1part2 } from "./day-1";
 
-test("should return 11 given the test input", ()  => {
-	expect(day1(testInput)).toBe(11);
+test("part 1 should return 11 given the test input", () => {
+	expect(day1part1(testInput)).toBe(11);
+});
+
+test("part 2 should return 31 given the test input", () => {
+	expect(day1part2(testInput)).toBe(31);
 });
--- days/1/day-1.ts
+++ days/1/day-1.ts
@@ -6,13 +6,26 @@ interface SplitList {
 	right: number[];
 }
 
-console.log("solution is:", day1(typedInput));
+console.log("part 1 solution is:", day1part1(typedInput));
+console.log("part 2 solution is:", day1part2(typedInput));
 
-export default function day1(rawInput: string): number {
+export function day1part1(rawInput: string): number {
 	const splitInput = splitInputLeftRight(rawInput);
 	let total = 0;
 	for (let i = 0; i < splitInput.left.length; i++) {
 		total += Math.abs(splitInput.left[i] - splitInput.right[i]);
+	}
+	return total;
+}
+
+export function day1part2(rawInput: string): number {
+	const splitInput = splitInputLeftRight(rawInput);
+	const rightFrequencyMap = getRightFrequencyMap(splitInput.right);
+	let total = 0;
+	for (let i = 0; i < splitInput.left.length; i++) {
+		const leftValue = splitInput.left[i];
+		const adjustedLeftValue = leftValue * (rightFrequencyMap[leftValue] || 0);
+		total += adjustedLeftValue;
 	}
 	return total;
 }
@@ -25,4 +38,15 @@ function splitInputLeftRight(input: string): SplitList {
 		result.right.push(Number(values[1]));
 	});
 	return { left: result.left.sort(), right: result.right.sort() };
+}
+
+function getRightFrequencyMap(rightInput: number[]): Record<number, number> {
+	return rightInput.reduce((acc, curr) => {
+		if (acc[curr]) {
+			acc[curr] = acc[curr] + 1;
+		} else {
+			acc[curr] = 1;
+		}
+		return acc;
+	}, {} as Record<number, number>);
 }
